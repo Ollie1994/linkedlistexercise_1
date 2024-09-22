5a4b75bf0d4e07f4b1e1c3c961222cd5136fc3ac
Ollie1994 <oliwer.carlsson.jobb@gmail.com>
2024-09-22 12:30:46 +0200

    finally progress

--- src/Citys.java
+++ src/Citys.java
@@ -1,74 +1,34 @@
 // https://www.geeksforgeeks.org/convert-linkedlist-to-string-in-java/
 
-import java.util.Iterator;
-import java.util.LinkedList;
-
 public class Citys {
-
 
     private String cityName;
     private int distanceFromGbInKm;
+
     public Citys(String cityName, int distanceFromGbInKm)
     {
         this.cityName = cityName;
         this.distanceFromGbInKm = distanceFromGbInKm;
     }
-    public String getCityName() {
-        return cityName;
-    }
-    public void setCityName(String cityName) {
-        this.cityName = cityName;
-    }
-    public int getDistanceFromGbInKm() {
-        return distanceFromGbInKm;
-    }
-    public void setDistanceFromGbInKm(int distanceFromGbInKm) {
-        this.distanceFromGbInKm = distanceFromGbInKm;
-    }
 
-
-
-    //@Override toString method
+    //---------------------Getters&Setters-----------------------------------------------
+    public String getCityName() {return cityName;}
+    public void setCityName(String cityName) {this.cityName = cityName;}
+    public int getDistanceFromGbInKm() {return distanceFromGbInKm;}
+    public void setDistanceFromGbInKm(int distanceFromGbInKm) {this.distanceFromGbInKm = distanceFromGbInKm;}
+    //--------------------------ToString--------------------------------
     public String toString()
     {
-        return "name='" + cityName + '\''  + '\'' + ", ID=" + distanceFromGbInKm;
+        return "City: " + cityName + ", Distance from Gothenburg: " + distanceFromGbInKm;
     }
+    //-----------------------------------------------------------------------------------------
+
+
+
 }
 
-class Main {
-    public static void main(String[] args)
-    {
-
-        LinkedList<Citys> citys = new LinkedList<>();
-
-        // adding elements to linked list
-        citys.add(new Citys("Ridhi", 202199));
-        citys.add(new Citys("Ananya", 202195));
-        citys.add(new Citys("Ankit", 202192));
-        citys.add(new Citys("Suraj", 202147));
-        citys.add(new Citys("Sarthak", 202178));
-        citys.add(new Citys("Sonika", 202167));
-        citys.add(new Citys("Shishya", 202157));
-
-        System.out.println("Using String Class\n");
-
-        // using string class
-        String s = "";
-
-        // using iterator for traversing a linked list
-        Iterator<Citys> itr = citys.iterator();
-
-        while (itr.hasNext()) {
-            // appending string using "+" operator
-            s = s + itr.next() + "\n";
-        }
-
-        // Displaying output
-        System.out.println(s);
 
 
-    }
-}
 
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,41 @@
-package PACKAGE_NAME;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.LinkedList;
 
 public class Main {
+    public static void main(String[] args) {
+
+        LinkedList<Citys> citys = new LinkedList<>();
+
+        // adding elements to linked list
+        citys.add(new Citys("Berlin", 582));
+        citys.add(new Citys("Gothenburg", 0));
+        citys.add(new Citys("Madrid", 2222));
+        citys.add(new Citys("Paris", 1161));
+        citys.add(new Citys("Rom", 1755));
+        citys.add(new Citys("Oslo", 251));
+        citys.add(new Citys("Warsaw", 838));
+        citys.sort(Comparator.comparingInt(Citys::getDistanceFromGbInKm));
+
+
+        // using string class
+        String s = "";
+
+        // using iterator for traversing a linked list
+        Iterator<Citys> itr = citys.iterator();
+
+        while (itr.hasNext()) {
+            // appending string using "+" operator
+            s = s + itr.next() + "\n";
+        }
+
+        // Displaying output
+        System.out.println(s);
+
+
+
+
+
+
+    }
 }
